586a0ec9f21f40a0d47fabe1a1e010e2cb5e0420
Thiago Gentil <88681055+Tgentil@users.noreply.github.com>
2023-04-24 14:01:16 -0300

    performance improve
    agora organiza o array por ordem decrescente antes de começar o código, quando a soma for maior que o resultado desejado ele pulará para o próximo caminho , e também agora ele mostra os valores utilizados para chegar ao resultado.

--- project/code/achar_soma.py
+++ project/code/achar_soma.py
@@ -1,14 +1,14 @@
 """script de soma de subconjuntos dentro de um array para obter um valor especifico"""
 
 # Importando os tipos de dados List e deque
-from typing import List
+from typing import List, Tuple
 from collections import deque
 import sys
 
 # Define o enconding
 sys.stdout.reconfigure(encoding='utf-8')
 
-def find_sum(numbers: List[float], target_sum: float, tolerance: float = 1e-6) -> bool:
+def find_sum(numbers: List[float], target_sum: float, tolerance: float = 1e-6) -> Tuple[bool, List[float]]:
     """
     Verifica se existe um subconjunto de `numbers` que soma exatamente `target_sum`.
 
@@ -19,43 +19,52 @@ def find_sum(numbers: List[float], target_sum: float, tolerance: float = 1e-6) -
         ponto flutuante.
 
     Returns:
-        Um valor booleano indicando se existe ou não um subconjunto que soma `target_sum`.
+        Um tuple com um valor booleano indicando se existe um subconjunto que soma `target_sum`
+        e uma lista com os números que formam o subconjunto.
     """
 
-    # Criação de uma fila (deque) com um único elemento (tupla com dois valores)
-    queue = deque([(0, 0)])
+    # Criação de uma fila (deque) com um único elemento (tupla com três valores)
+    queue = deque([(0, 0, [])])
 
     # Loop enquanto a fila tiver elementos
     while queue:
-        # Remove o último elemento da fila e atribuição dos valores às variáveis index e partial_sum
-        index, partial_sum = queue.pop()
+        # Remove o último elemento da fila e atribuição dos valores
+        index, partial_sum, subset = queue.pop()
 
         # Verifica se a diferença entre partial_sum e target_sum é menor que a tolerância especifica
         if abs(partial_sum - target_sum) < tolerance:
-            return True  # Retorna True se a soma for encontrada
+            return True, subset  # Retorna True e o subconjunto se a soma for encontrada
 
         # Verifica se index é maior ou igual ao comprimento da lista numbers
         if index >= len(numbers):
-            continue # Vai para o proximo elemento
+            continue # Vai para o próximo elemento
 
-        # Adiciona duas novas tuplas à fila, uma com o próximo índice e partial_sum
-        # e outra com o próximo índice e partial_sum + o número atual
-        queue.append((index+1, partial_sum))
-        queue.append((index+1, partial_sum + numbers[index]))
+        # Verifica se partial_sum atual mais o próximo número é menor ou igual ao target_sum
+        if partial_sum + numbers[index] <= target_sum:
+            # Adiciona duas novas tuplas à fila, uma com o próximo índice, partial_sum e subset
+            # e outra com o próximo índice, partial_sum + o número atual e subset + [numbers[index]]
+            queue.append((index+1, partial_sum, subset))
+            queue.append((index+1, partial_sum + numbers[index], subset + [numbers[index]]))
 
-    return False  # Retorna False se a soma não for encontrada
+    return False, []  # Retorna False e uma lista vazia se a soma não for encontrada
+
 
 
 # Verifica se o script está sendo executado diretamente
 if __name__ == '__main__':
     NUMBERS = [0.50, 2, 2.50, 5, 7, 2.20]  # Exemplo de números
+
     TARGET_SUM = 10.00  # Exemplo de soma desejada
+
+    # Remove os termos maiores que a soma desejada e ordena o array em ordem decrescente
+    NUMBERS = sorted([num for num in NUMBERS if num <= TARGET_SUM], reverse=True)
 
 # Executa a função find_sum com os números e soma desejada especificados
     RESULT = find_sum(NUMBERS, TARGET_SUM)
 
     # Imprime uma mensagem indicando que o valor desejado foi ou não foi encontrado
-    if RESULT:
-        print("\nEXISTE !!")
+    if RESULT[0]:
+        print(f"\nEXISTE !! Subconjunto: {RESULT[1]} ")
     else:
         print("\nnão tem :(")
+
